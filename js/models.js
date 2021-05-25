1bcbbab9c70e4fe9cfc264d599f9161cf796ad52
Connor Dieck <connordieck@gmail.com>
2021-05-25 17:20:27 -0400

    updated getURL function in models.js

--- js/models.js
+++ js/models.js
@@ -23,8 +23,8 @@ class Story {
 	/** Parses hostname out of URL and returns it. */
 
 	getHostName() {
-		// UNIMPLEMENTED: complete this function!
-		return 'hostname.com';
+		// UPDATED
+		return this.url;
 	}
 }
 
